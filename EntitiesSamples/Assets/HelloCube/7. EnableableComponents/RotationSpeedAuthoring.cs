a5570076e8310495a88553172e716a8b90e25e60
Matt Barr <matt@mysidewalk.com>
2024-07-07 13:28:50 -0500

    Updates to enableable components scene (added per cube interval/timer for enabling/disabling).

--- EntitiesSamples/Assets/HelloCube/7. EnableableComponents/RotationSpeedAuthoring.cs
+++ EntitiesSamples/Assets/HelloCube/7. EnableableComponents/RotationSpeedAuthoring.cs
@@ -1,23 +1,34 @@
 using UnityEngine;
 using Unity.Entities;
 using Unity.Mathematics;
+using UnityEngine.Serialization;
 
 namespace HelloCube.EnableableComponents {
     public class RotationSpeedAuthoring : MonoBehaviour {
-        public bool StartEnabled;
-        public float DegreesPerSecond = 360.0f;
+        public bool startEnabled;
+        public float degreesPerSecond = 360.0f;
+        public float interval = 1.3f;
 
         public class Baker : Baker<RotationSpeedAuthoring> {
             public override void Bake(RotationSpeedAuthoring authoring) {
                 var entity = GetEntity(TransformUsageFlags.Dynamic);
 
-                AddComponent(entity, new RotationSpeed { RadiansPerSecond = math.radians(authoring.DegreesPerSecond) });
-                SetComponentEnabled<RotationSpeed>(entity, authoring.StartEnabled);
+                AddComponent(
+                    entity,
+                    new RotationSpeed {
+                        radiansPerSecond = math.radians(authoring.degreesPerSecond),
+                        interval = authoring.interval,
+                        timeRemaining = authoring.interval,
+                    }
+                );
+                SetComponentEnabled<RotationSpeed>(entity, authoring.startEnabled);
             }
         }
     }
 
     struct RotationSpeed : IComponentData, IEnableableComponent {
-        public float RadiansPerSecond;
+        public float radiansPerSecond;
+        public float interval;
+        public float timeRemaining;
     }
 }
--- EntitiesSamples/Assets/HelloCube/7. EnableableComponents/RotationSystem.cs
+++ EntitiesSamples/Assets/HelloCube/7. EnableableComponents/RotationSystem.cs
@@ -1,36 +1,29 @@
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Transforms;
 
 namespace HelloCube.EnableableComponents {
     public partial struct RotationSystem : ISystem {
-        float timer;
-        const float interval = 1.3f;
-
         [BurstCompile]
-        public void OnCreate(ref SystemState state) {
-            timer = interval;
-            state.RequireForUpdate<ExecuteEnableableComponents>();
-        }
+        public void OnCreate(ref SystemState state) { state.RequireForUpdate<ExecuteEnableableComponents>(); }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state) {
             var deltaTime = SystemAPI.Time.DeltaTime;
-            timer -= deltaTime;
 
             // Toggle the enabled state of every RotationSpeed
-            if (timer < 0) {
-                foreach (var rotationSpeedEnabled in SystemAPI.Query<EnabledRefRW<RotationSpeed>>()
-                    .WithOptions(EntityQueryOptions.IgnoreComponentEnabledState)) {
-                    rotationSpeedEnabled.ValueRW = !rotationSpeedEnabled.ValueRO;
-                }
-
-                timer = interval;
+            foreach (var (enabled, speed) in SystemAPI.Query<EnabledRefRW<RotationSpeed>, RefRW<RotationSpeed>>()
+                .WithOptions(EntityQueryOptions.IgnoreComponentEnabledState)) {
+                speed.ValueRW.timeRemaining -= deltaTime;
+                if (!(speed.ValueRW.timeRemaining <= 0.0f)) { continue; }
+                enabled.ValueRW = !enabled.ValueRW;
+                speed.ValueRW.timeRemaining = speed.ValueRW.interval;
             }
 
             // The query only matches entities whose RotationSpeed is enabled.
             foreach (var (transform, speed) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotationSpeed>>()) {
-                transform.ValueRW = transform.ValueRO.RotateY(speed.ValueRO.RadiansPerSecond * deltaTime);
+                transform.ValueRW = transform.ValueRW.RotateY(speed.ValueRO.radiansPerSecond * deltaTime);
             }
         }
     }
